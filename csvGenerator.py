29f9b50fbb9d8ae164068112b4daaaf1178a6609
César Pérez Canseco <perez.canseco@gmail.com>
2022-08-19 17:43:30 -0500

    input file generator

---
+++ csvGenerator.py
@@ -0,0 +1,23 @@
+import time
+from random import random
+
+FILE_NAME = "input.csv"
+
+DATE_FORMAT = '%Y/%m/%d'
+END_DATE = time.time()
+LIMIT_TRANSACTION = 10000
+
+init_date = time.mktime(time.strptime('2020/1/1',DATE_FORMAT))
+
+with open(FILE_NAME, "w") as out:
+    for i in range(100):
+        randtime = init_date + random() * 2000000.0
+        randtime = END_DATE if randtime > END_DATE else randtime
+        randdate = time.strftime(DATE_FORMAT, time.localtime(randtime))
+        
+        transaction = random() * LIMIT_TRANSACTION
+        transaction *= -1 if random() > 0.5 else transaction
+
+        out.write('{},{},{:.2f}\n'.format(i + 1, randdate, transaction))
+
+        init_date = randtime
