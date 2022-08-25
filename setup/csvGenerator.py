b887c7984fbdfe0d9a6ad58fea517e8959656a9b
César Pérez Canseco <perez.canseco@gmail.com>
2022-08-25 01:19:36 -0500

    Automated testing script redesigned

--- setup/csvGenerator.py
+++ setup/csvGenerator.py
@@ -1,25 +1,24 @@
-import sys
 import time
 from random import random
-
-FILE_NAME = sys.argv[1] if len(sys.argv) > 1 else "input.csv"
 
 DATE_FORMAT = '%Y-%m-%d'
 END_DATE = time.time()
 LIMIT_TRANSACTION = 10000
 
-init_date = time.mktime(time.strptime('2020-1-1',DATE_FORMAT))
 
-with open(FILE_NAME, "w") as out:
-    n = 100 if len(sys.argv) <= 2 else int(sys.argv[2])
-    for i in range(n):
-        randtime = init_date + random() * 2000000.0
-        randtime = END_DATE if randtime > END_DATE else randtime
-        randdate = time.strftime(DATE_FORMAT, time.localtime(randtime))
-        
-        transaction = random() * LIMIT_TRANSACTION
-        transaction *= -1 if random() > 0.5 else transaction
+def create(fileName="input.csv", n=100):
+    init_date = time.mktime(time.strptime('2020-1-1',DATE_FORMAT))
 
-        out.write('{},{},{:.2f}\n'.format(i + 1, randdate, transaction))
+    with open(fileName, "w") as out:
+        for i in range(n):
+            randtime = init_date + random() * 2000000.0
+            randtime = END_DATE if randtime > END_DATE else randtime
+            randdate = time.strftime(DATE_FORMAT, time.localtime(randtime))
+            
+            transaction = random() * LIMIT_TRANSACTION
+            transaction *= -1 if random() > 0.5 else transaction
 
-        init_date = randtime
+            out.write('{},{},{:.2f}\n'.format(i + 1, randdate, transaction))
+
+            init_date = randtime
+
